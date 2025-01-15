3148588e4f0ba5ecfeb23f25f551c8b3ca8c1344
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2025-01-16 00:35:34 +0400

    add @media

--- src/main/java/com/example/korzhbirsday/KorzhBirsdayApplication.java
+++ src/main/java/com/example/korzhbirsday/KorzhBirsdayApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KorzhBirsdayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KorzhBirsdayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(KorzhBirsdayApplication.class, args);
+    }
 
 }
--- src/main/java/com/example/korzhbirsday/controller.java
+++ src/main/java/com/example/korzhbirsday/controller.java
@@ -1,4 +1,10 @@
 package com.example.korzhbirsday;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class controller {
+    public String home(){
+        return "index";
+    }
 }
